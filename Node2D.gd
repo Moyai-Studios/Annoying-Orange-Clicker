a0bc4ff389d4ae56c96cfff49c98891a8c692ce8
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2021-11-27 18:11:00 -0500

    1.0.1 Source Commit

--- Node2D.gd
+++ Node2D.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
+var subscribers = 0
 var points = 0
-var pointsovertime = 0
+var subsovertime = 0
 var bushprice = 25
 var treeprice = 25
-var pointsperclick = 1
+var subsperclick = 1
+var mouseonbush = 0
+var mouseontree = 0
 var _timer = null
 
 func _ready():
-	$canvas/RichTextLabel.text = str(points) + " Points\n" + str(pointsovertime) + " Points Per Second"
-	$canvas/bushprice.text = str(round(bushprice))
-	$canvas/treeprice.text = str(round(treeprice))
+	$canvas/RichTextLabel.text = str(points) + "$\n" + str(subscribers) + " Subscribers\n"  + str(subsovertime) + " Subscribers over time"
+	$canvas/bushprice.text = str(round(bushprice)) + "$"
+	$canvas/treeprice.text = str(round(treeprice)) + "$"
+	$canvas/ItemInfo.text = ""
 	# Create a timer node
 	var timer = Timer.new()
 
@@ -30,26 +34,55 @@ func _ready():
 
 
 func _on_TextureButton_pressed():
-	points += pointsperclick
-	$canvas/RichTextLabel.text = str(points) + " Points\n" + str(pointsovertime) + " Points Per Second"
+	subscribers += subsperclick
+	$canvas/RichTextLabel.text = str(points) + "$\n" + str(subscribers) + " Subscribers\n"  + str(subsovertime) + " Subscribers over time"
 
 func _on_bush_pressed():
+	## bush will be replaced by marketing or increase video quality in the future
 	if points >= bushprice:
-		pointsovertime += 1
+		$canvas/AudioStreamPlayer2D.play()
+		subsovertime += 1
 		points -= bushprice
 		bushprice += bushprice / 2
-		$canvas/RichTextLabel.text = str(points) + " Points\n" + str(pointsovertime) + " Points Per Second"
-		$canvas/bushprice.text = str(round(bushprice))
+		$canvas/RichTextLabel.text = str(points) + "$\n" + str(subscribers) + " Subscribers\n"  + str(subsovertime) + " Subscribers over time"
+		$canvas/bushprice.text = str(round(bushprice*100)/100) + "$"
 	
 func repeat_me():
-	points += pointsovertime
-	$canvas/RichTextLabel.text = str(points) + " Points\n" + str(pointsovertime) + " Points Per Second"
+	points += subscribers * 0.01
+	subscribers += subsovertime
+	$canvas/RichTextLabel.text = str(points) + "$\n" + str(subscribers) + " Subscribers\n"  + str(subsovertime) + " Subscribers over time"
 
 
 func _on_tree_pressed():
 	if points >= treeprice:
-		pointsperclick += 1
+		$canvas/AudioStreamPlayer2D.play()
+		subsperclick += 1
 		points -= treeprice
 		treeprice += treeprice / 2
-		$canvas/RichTextLabel.text = str(points) + " Points\n" + str(pointsovertime) + " Points Per Second"
-		$canvas/treeprice.text = str(round(treeprice))
+	$canvas/RichTextLabel.text = str(points) + "$\n" + str(subscribers) + " Subscribers\n" + str(subsovertime) + " Subscribers over time"
+	$canvas/treeprice.text = str(round(treeprice)) + "$"
+
+
+func _on_TextureButton_mouse_entered():
+	$canvas/RichTextLabel.text = str(points) + "$\n" + str(subscribers) + " Subscribers\n" + str(subsovertime) + " Subscribers over time"
+
+
+func _on_bush_mouse_entered():
+	mouseonbush = 1
+	$canvas/ItemInfo.text = "This bush will be marketing/video quality in the future. Each you buy will increase your subscribers by one every second."
+
+func _on_bush_mouse_exited():
+	mouseonbush = 0
+	if mouseonbush == 0 && mouseontree == 0:
+		$canvas/ItemInfo.text = " "
+
+
+func _on_tree_mouse_entered():
+	mouseontree = 1
+	$canvas/ItemInfo.text = "This tree will be something else in the future. Each you buy will increase your subscribers per click by one."
+
+
+func _on_tree_mouse_exited():
+	mouseontree = 0
+	if mouseonbush == 0 && mouseontree == 0:
+		$canvas/ItemInfo.text = " "
