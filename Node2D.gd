e46a533d76ffe61cf0e8a45ee3ae39580bdf49a5
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2022-01-17 02:13:28 -0500

    1.0.2 Source Commit
    This version be buggy but I'll upload it anyways for the lolz lmao

--- Node2D.gd
+++ Node2D.gd
@@ -8,13 +8,18 @@ var treeprice = 25
 var subsperclick = 1
 var mouseonbush = 0
 var mouseontree = 0
+var sponsor = 0
+var rand_generate = RandomNumberGenerator.new()
 var _timer = null
 
 func _ready():
 	$canvas/RichTextLabel.text = str(points) + "$\n" + str(subscribers) + " Subscribers\n"  + str(subsovertime) + " Subscribers over time"
-	$canvas/bushprice.text = str(round(bushprice)) + "$"
-	$canvas/treeprice.text = str(round(treeprice)) + "$"
+	$canvas/bushprice.text = str(round(bushprice)) + "$ For Marketing"
+	$canvas/treeprice.text = str(round(treeprice)) + "$ For Video Quality"
 	$canvas/ItemInfo.text = ""
+	$canvas/Sponsor.visible = false
+	randomize()
+	rand_generate.randomize()
 	# Create a timer node
 	var timer = Timer.new()
 
@@ -45,11 +50,17 @@ func _on_bush_pressed():
 		points -= bushprice
 		bushprice += bushprice / 2
 		$canvas/RichTextLabel.text = str(points) + "$\n" + str(subscribers) + " Subscribers\n"  + str(subsovertime) + " Subscribers over time"
-		$canvas/bushprice.text = str(round(bushprice*100)/100) + "$"
+		$canvas/bushprice.text = str(round(bushprice*100)/100) + "$ For Marketing"
 	
 func repeat_me():
 	points += subscribers * 0.01
 	subscribers += subsovertime
+	if round(rand_generate.randf_range(0.0, 100.0)) == 5:
+		if sponsor == 0:
+			$canvas/Sponsor.visible = true
+			$canvas/Sponsor.rect_position.x = round(rand_generate.randf_range(0.0, 900.0))
+			$canvas/Sponsor.rect_position.y = round(rand_generate.randf_range(0.0, 400.0))
+			sponsor = 1
 	$canvas/RichTextLabel.text = str(points) + "$\n" + str(subscribers) + " Subscribers\n"  + str(subsovertime) + " Subscribers over time"
 
 
@@ -60,7 +71,7 @@ func _on_tree_pressed():
 		points -= treeprice
 		treeprice += treeprice / 2
 	$canvas/RichTextLabel.text = str(points) + "$\n" + str(subscribers) + " Subscribers\n" + str(subsovertime) + " Subscribers over time"
-	$canvas/treeprice.text = str(round(treeprice)) + "$"
+	$canvas/treeprice.text = str(round(treeprice)) + "$ For Video Quality"
 
 
 func _on_TextureButton_mouse_entered():
@@ -86,3 +97,11 @@ func _on_tree_mouse_exited():
 	mouseontree = 0
 	if mouseonbush == 0 && mouseontree == 0:
 		$canvas/ItemInfo.text = " "
+
+
+func _on_Sponsor_pressed():
+	if sponsor == 1:
+		points += (subscribers + 10) * round(rand_generate.randf_range(1.0, 3.0))
+		subscribers += subscribers * round(rand_generate.randf_range(0.03, 0.1))
+		$canvas/Sponsor.visible = false
+		sponsor = 0
